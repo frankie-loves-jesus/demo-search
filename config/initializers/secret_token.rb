8636bb93d0bef78d7bfd1f8cd1f2b02aacb13575
Frankie <orangeprince@hushmail.com>
2014-03-21 02:53:34 +0100

    rails new demo-search

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoSearch::Application.config.secret_key_base = '8194b3115101b58a974eb592761d116371ccaf91364fa5d9ac8128188872d608255a349ce99457d29291f0310301200d85a62b5d5e43870935c8eddf1dc744b2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DemoSearch::Application.config.session_store :cookie_store, key: '_demo-search_session'
