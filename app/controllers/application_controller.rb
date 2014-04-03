68dfccea5aabb2ab98dc2c1497855631a664db4d
Frankie <orangeprince@hushmail.com>
2014-04-03 22:53:03 +0200

    Make Devise use strong parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def forem_user
     current_user
@@ -8,4 +9,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:email) { |u| u.permit({ roles: [] }, :password, :password_confirmation, :remember_me) }
+  end
 end
